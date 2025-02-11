5608c8fbda2738ee1419f37bbd0d7539cb97aabf
Ryotaro Sakai <sakai@cloudbuilders.jp>
2025-02-11 15:26:20 +0000

    2nd commit

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,3 @@ FROM debian:bookworm-slim
 COPY --from=ghcr.io/astral-sh/uv:0.5.30 /uv /uvx /bin/
 
 WORKDIR /app
-COPY . .
-
-RUN uv sync --frozen
--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+import boto3
+
+
 def main():
     print("Hello from pytmpl!")
 
 
 if __name__ == "__main__":
     main()
+
+    client = boto3.client("sts")
+
+    # Retrieves all regions/endpoints that work with EC2
+    response = client.get_caller_identity()
+    print("Response:", response["UserId"])
